986a38c4fac72e3155d06c80e8b4bb43d0939ce9
JP-Moraes <foxcraft2030@gmail.com>
2025-05-01 18:29:59 -0300

    Atualizar o Dungeon Crawler.c

--- Dungeon Crawler.c
+++ Dungeon Crawler.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h> 
+#include <conio.h>
+#include <windows.h>
 
 //A biblioteca conio.he esta aqui para auxiliar na movimentçãoem a necessidade do enter - Funções: _kbhit() e _getch() 
 
 int escolha1; 
 int escolha;
 
+//int x, u - posição do jogador 
+//int Altura, Comprimento - coordenadas na matriz 
 
 
 // ------ Matriz do Jogo ---------------------
@@ -22,17 +25,15 @@ int escolha;
 // }          if (x == i && y == j) {
 // }              printf ("P "); - marcando posicao do jogador
 // }          else {
-// }              printf (". "); - marcando o grid
+// }              printf (". "); - marcando o espaço vazio 
 // }          }
-// }      printf ("\n"); 
+// }      printf ("\n");
 
+// -- Capturar o teclado para movimento --
 
-// ------- Função Controle e Colisão -------
-
-// void controle() {
 //int novoX = x;
 //int novoY = y; - Atualização das coordenadas na matriz
-//char comando; uso do teclado para 
+//char comando;- uso do teclado para movimentar
 
 //if (_kbhit()) {
 //comando = _getch(); - Lê a tecla pressionada
@@ -45,9 +46,10 @@ int escolha;
 // else if (comando == D || comando == d) 
 //  novoy--;
 
+-- Limitação da movimentação e colisão -- 
 // if (novoX >= 0 && novoX < Altura && novoY >= 0 && novoY < Comprimento)
-//. x = novoX; 
-//. y = novoY;
+// x = novoX; 
+// y = novoY; - Atualiza para a nova posição 
 
 
 
